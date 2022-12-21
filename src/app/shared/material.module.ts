04ff74a6862bbf819cf91b19d123520e9b5454c4
Aashish Chavan <aashish.chavan@fisglobal.com>
2022-12-21 17:43:49 +0530

    First Commit

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }

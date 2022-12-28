db989afabf24b580a44523eb829ab5dacd4d8986
Aashish Chavan <aashish.chavan@fisglobal.com>
2022-12-28 20:18:42 +0530

    Build Error

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -50,7 +50,7 @@ const routes: Routes = [
     MainContentComponent,
     SidenavComponent,
     NotesComponent,
-    NewContactDialogComponent
+    NewContactDialogComponent,
     NotesComponent
   ],
   imports: [

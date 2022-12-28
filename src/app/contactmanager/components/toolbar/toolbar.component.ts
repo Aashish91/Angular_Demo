e1d74b6601652c7e8619024fb047c798bb8e75ac
Aashish Chavan <aashish.chavan@fisglobal.com>
2022-12-28 19:38:17 +0530

    New Contact Dialog, Cutom Themes, Toggle Themes & Dir

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { NewContactDialogComponent } from '../new-contact-dialog/new-contact-dialog.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,4 +12,29 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class ToolbarComponent {
 
   @Output() toggleSideNav = new EventEmitter<void>();
+  @Output() toggleTheme = new EventEmitter<void>();
+  @Output() toggleDir = new EventEmitter<void>();
+  constructor(public dialog: MatDialog, private snackBar: MatSnackBar, private router: Router) {
+  }
+
+  openAddContactDialog() {
+    let dialogRef = this.dialog.open(NewContactDialogComponent, {
+      width: '450 px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog was closed', result);
+      if (result) {
+        let snackBarRef = this.openSnackBar("Contact Added", "Navigate").onAction().subscribe(() => {
+          this.router.navigate(['/contactmanager', result.id]);
+        });
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, action, {
+      duration: 5000,
+    });
+  }
 }
